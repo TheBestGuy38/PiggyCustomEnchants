4024e969ecb56eac7e4fd78b55c7d0059e7ac452
TheBestGuy38 <52821669+TheBestGuy38@users.noreply.github.com>
2019-07-14 22:58:09 +0800

    Create CustomEnchantIds.php

---
+++ src/DaPigGuy/PiggyCustomEnchants/CustomEnchants/CustomEnchantIds.php
@@ -0,0 +1,41 @@
+<?php
+namespace DaPigGuy\PiggyCustomEnchants\CustomEnchants;
+/**
+ * Class CustomEnchants
+ * @package PiggyCustomEnchants\CustomEnchantsIds
+ */
+class CustomEnchantsIds
+{
+    //GLOBAL
+    const LIFESTEAL = 100;
+    const BLIND = 101;
+    const DEATHBRINGER = 102;
+    const POISON = 104;
+    /*const BLOCK = 105;
+    const ICEASPECT = 106;
+    const SHOCKWAVE = 107;*/
+    //const THUNDERBLOW = 110;
+    const WITHER = 115;
+    //const HEADLESS = 116;
+    const SOULBOUND = 118;
+    const LIGHTNING = 123;
+    //TOOLS
+    const SMELTING = 201;
+    const ENERGIZING = 202;
+    const QUICKENING = 203;
+    const HASTE = 207;
+    //ARMOR
+    const CURSED = 408;
+    const ENDERSHIFT = 409;
+    const DRUNK = 410;
+    const OVERLOAD = 419;
+    const HEAVY = 422;
+    const SHIELDED = 423;
+    //HELMET
+    const IMPLANTS = 600;
+    const GLOWING = 601;
+    //CHESTPLATE
+    //BOOTS
+    const GEARS = 500;
+    const SPRINGS = 501;
+}
